954bafbe982125033e8a260c1c07d2953c3ca91b
jovanlekic12 <lekicjovan1212@gmail.com>
2024-06-21 20:29:17 +0200

    add class

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
 import "./style.css";
+
+const btnEasy = document.querySelector(".btn__easy");
+const btnMedium = document.querySelector(".btn__medium");
+const btnHard = document.querySelector(".btn__hard");
+
+// game
+
+class SquareManager {
+  squares;
+  constructor() {
+    this.squares = [];
+  }
+}
+
+class Square {
+  id;
+  isClicked;
+  constructor() {
+    this.id = self.crypto.randomUUID();
+    this.isClicked = false;
+  }
+}
