54c6a5aa1e927dafb3162ad9a0a9e9b79167f6fb
jovanlekic12 <lekicjovan1212@gmail.com>
2024-06-22 01:10:44 +0200

    add buttons

--- main.js
+++ main.js
@@ -3,8 +3,55 @@ import "./style.css";
 const btnEasy = document.querySelector(".btn__easy");
 const btnMedium = document.querySelector(".btn__medium");
 const btnHard = document.querySelector(".btn__hard");
+const gridContainer = document.querySelector(".square__container");
 
-// game
+function getRandomColor() {
+  let letters = "0123456789ABCDEF";
+  let randomColor = "#";
+  for (let i = 0; i < 6; i++) {
+    randomColor += letters[Math.floor(Math.random() * 16)];
+  }
+  return randomColor;
+}
+function makeRows(rows, cols) {
+  gridContainer.innerHTML = "";
+
+  for (let c = 0; c < rows * cols; c++) {
+    let cell = document.createElement("div");
+    cell.classList.add("square");
+    gridContainer.appendChild(cell);
+  }
+  gridContainer.style.gridTemplateColumns = `repeat(${cols},1fr`;
+  gridContainer.style.gridTemplateRows = `repeat(${rows},1fr)`;
+}
+makeRows(3, 3);
+
+const squares = gridContainer.querySelectorAll(".square");
+squares.forEach((square) => (square.style.backgroundColor = getRandomColor()));
+
+btnEasy.addEventListener("click", function () {
+  makeRows(3, 3);
+  const squares = gridContainer.querySelectorAll(".square");
+  squares.forEach(
+    (square) => (square.style.backgroundColor = getRandomColor())
+  );
+});
+
+btnMedium.addEventListener("click", function () {
+  makeRows(4, 4);
+  const squares = gridContainer.querySelectorAll(".square");
+  squares.forEach(
+    (square) => (square.style.backgroundColor = getRandomColor())
+  );
+});
+
+btnHard.addEventListener("click", function () {
+  makeRows(5, 5);
+  const squares = gridContainer.querySelectorAll(".square");
+  squares.forEach(
+    (square) => (square.style.backgroundColor = getRandomColor())
+  );
+});
 
 class SquareManager {
   squares;
